f8f3052bfb8c6a80a9b011b755396210a5c6c09f
Farkhan1220 <farkhanamarrudin8@gmail.com>
2024-12-31 00:58:56 +0700

    Update klasifikasi_cuaca_streamlit.py

--- klasifikasi_cuaca_streamlit.py
+++ klasifikasi_cuaca_streamlit.py
@@ -1,12 +1,10 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+
 
 # Streamlit App Title
 st.title("Weather Classification Analysis")
