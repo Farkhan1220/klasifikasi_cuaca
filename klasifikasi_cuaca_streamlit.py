c019fd81fe25ebe0df64cd728a2eea970863aae8
Farkhan1220 <farkhanamarrudin8@gmail.com>
2024-12-31 00:45:56 +0700

    Update klasifikasi_cuaca_streamlit.py

--- klasifikasi_cuaca_streamlit.py
+++ klasifikasi_cuaca_streamlit.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
